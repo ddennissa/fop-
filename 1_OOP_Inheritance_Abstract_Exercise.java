b96f513d2823019b81f351d31d24bec2b7750806
Khaled Jallouli <jallouli.khaled@yahoo.fr>
2023-12-18 19:24:15 +0100

    Inheritance and abstract Session and Exercises

---
+++ 1_OOP_Inheritance_Abstract_Exercise.java
@@ -0,0 +1,25 @@
+/* Exercise: 
+
+1. Person Class
+  Define a Person() as abstract class.
+  In the constructor, define several attributes of a person (name, age, place of birth)
+  define the getters and setters
+  Write methods:
+    - introduce_yourself(). This method would print out a statement such as, "Hello, my name is ..,my age is .. and my place of birth is .."
+    - birthday_person(): add 1 to the person's age.
+
+2. Student Class
+  Define a Student() class inherites from Person class.
+  Define more attributes than the Person class: department, a graduation year, a gpa.
+  define the getters and setters
+  Override method: 
+    - introduce_yourself(). This method would print out a statement such as, "Hello, my name is ,my age is .. and my place of birth is .., my department is,.."
+    - Add a method passed_year() if he has gpa >= 4. (return True or False) 
+
+3. Main: 
+Instantiate Student class
+Introduce the student,
+Celebrate the student birthday ,
+Call passed_year(): if it's true => Congradulations! if false = Good luck for next time
+
+ */
