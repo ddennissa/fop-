52d65de3cb078da71116a53bcbf0c163c2aef228
Khaled Jallouli <jallouli.khaled@yahoo.fr>
2024-01-22 21:30:21 +0100

    association and exercise

--- 2_OOP_Association.java
+++ 2_OOP_Association.java
@@ -1,27 +1,36 @@
-class Course {
-    private int idCourse;
-    private String nameCourse;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Course(int idCourse, String nameCourse) {
-        this.idCourse = idCourse;
-        this.nameCourse = nameCourse;
+class Course {
+    private int id;
+    private String name;
+
+    public Course(int id, String name) {
+        this.id = id;
+        this.name = name;
         helloCourse();
     }
 
     public void helloCourse() {
         System.out.println("Welcome to the course");
     }
-    public int getIdCourse() {
-        return idCourse;
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
     }
 
 }
 
-abstract class Student {
+public class Student {
     protected int age;
+    protected Course course;
 
-    public Student(int age) {
+    public Student(int age, Course course) {
         this.age = age;
+        this.course = course;
     }
 
     public void haveABirthday() {
@@ -38,51 +47,55 @@ abstract class Student {
         this.age = age;
     }
 
-    public abstract void decreaseAge();
+    public Course getCourse() {
+        return this.course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    } 
+
+    public void hello_student(){
+        System.out.println("I am a student");
+        this.course.helloCourse();
+    }
+
+    public boolean older_student(Student second_student){
+        if (this.age > second_student.age)
+            return true;
+        else
+           return false;
+    }
+
+    public void studentCourse(){
+        int student_course_id = this.course.getId();  // this.course.id; NOT ALLOWED because id is an attribute of Course
+        System.out.println("Course id of the student" + student_course_id);
+        System.out.println("Course name of the student" + this.course.getName());
+
+    }
 }
 
-class MathStudent extends Student {
-    private String favouriteClass;
-
-    public MathStudent(int age, String favouriteClass) {
-        super(age);
-        this.favouriteClass = favouriteClass;
-    }
-
-    public void mathStudentDef() {
-        System.out.println("I am a math student");
-    }
-
-    @Override
-    public void decreaseAge() {
-        int decreaseAge = 5;
-        this.age -= decreaseAge;
-        //this.setAge(this.getAge() - decreaseAge); // More useful: Association: whe you call a class inside another class
-
-
-    }
-}
-
-
-class Professor {
-    private String nameProf;
-
-    public Professor(String nameProf) {
-        this.nameProf = nameProf;
-    }
-
-    public void profCourse() {
-        System.out.println("Course id of the prof: "); // TODO print course id 
-        System.out.println("Course name of the prof: ");  // TODO print course name
-    }
-
-}
 
 public class Main {
     public static void main(String[] args) {
-        // TODO Create Course
-        // TODO Create Professor
-        // Access Professor Course
-        // Add method to Studen
+        Course programming = new Course(0, "Java");
+        Student student_k = new Student(20, programming);
+        // Display course name of the student
+        String student_course_name = student_k.getCourse().getName();
+        System.out.println(student_course_name);
+        student_k.studentCourse();
+        student_k.getCourse().helloCourse();
+
+        // Lists
+        List<String> names = new ArrayList<>();
+        names.add("Alex");
+        names.add("Max");
+
+        int size = names.size(); // The length / Size of the list
+        for (String name: names){
+            System.out.println(name);        
+        }
+        
+        // student_k.older_student(student_xx);
     }
 }
---
+++ 2_OOP_Association_Exercise.java
@@ -0,0 +1,31 @@
+/*
+
+1. Car Class that have a model, year, color, fuelLevel, engineRunning.
+- startEngine(): start the engine if not started and fuelLevel is higher than 0. If engine already started, display that it is already running. Otherwise, display that it is not possible to start it
+- stopEngine(): stop the engine if it is running otherwise display that it is already stopped
+- refuel(double amount): add fuel if the amount is higher than 0
+- display_information(): display car information
+
+
+
+2. Client Class
+ Define a Client() class inherites from Person class (from Exercise 1).
+Add another attribut: a CAR!
+Override method: 
+- introduce_yourself(). This method would print out a statement such as, "Hello, my name is , my age is .. and my place of birth is .., Thoe model is my care is ... with a ... color"
+- Add a method client_type(): if the year of his car > 2010 : print new client else print old client.
+- Add a method test_car():
+    displayInfo()
+    startEngine()
+    refuel(20.0)
+    startEngine()
+    displayInfo()
+    stopEngine()
+
+
+3. Create two Clients and two cars. Add a method to compare the cars of two clients.   
+
+
+ENJOY :)
+ 
+ */
