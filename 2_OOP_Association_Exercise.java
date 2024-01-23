ddad2e0d604f13c62ace1d6218a7b3c9be585b42
ddennissa <154242405+ddennissa@users.noreply.github.com>
2024-01-23 19:54:33 +0100

    Update 2_OOP_Association_Exercise.java

--- 2_OOP_Association_Exercise.java
+++ 2_OOP_Association_Exercise.java
@@ -29,51 +29,143 @@ ENJOY :)
  
  */
 // Class and attributes 
-class Car {
-private int model;
-private String color;
-private int fuelLevel;
-private boolean engineRunning;
-//constructor 
-public Car (int model, String color, int fuelLevel, boolean engineRunning) {
- this.model = model;
- this.color = color;
- this.fuelLevel = fuelLevel;
- this.engineRunning = engineRunning;
-}
-//getters and setters 
-public int getModel(){
- return this.model;
-}
-public String getColor(){
- return this.color();
-}
-public int getFuelLevel(){
- return this.fuelLevel;
-} 
-public boolean getEngineRunning(){
- return this.engineRunning;
+
+public class Car {
+    private String model;
+    private int year;
+    private String color;
+    private double fuelLevel;
+    private boolean engineRunning;
+    //constructor
+    public Car (String model, int year, String color, int fuelLevel, boolean engineRunning) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.fuelLevel = 0.0;
+        this.engineRunning = false;
+    }
+    //getters and setters
+    public String getModel(){
+        return model;
+         }
+    public String getColor(){
+        return color; //Question : return this.color; or return color in getter method
+    }
+    public int getYear(){
+        return year;
+    }
+    public double getFuelLevel(){
+        return fuelLevel;
+    }
+    public boolean getEngineRunning(){
+        return engineRunning;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+    public void setFuelLevel(double fuelLevel){
+        this.fuelLevel = fuelLevel;
+    }
+    public void setEngineRunning(boolean engineRunning){
+        this.engineRunning = engineRunning;
+    }
+    //methods
+    public void startEngine(){
+        if(!engineRunning && fuelLevel > 0){
+            engineRunning = true;
+        } else if (engineRunning) {
+            System.out.println("Engine is running!");
+        } else {
+            System.out.println("Not possible!");
+        }
+    }
+    public void stopEngine(){
+        if(engineRunning){
+            engineRunning = false;
+        } else {
+            System.out.println("Engine is already stopped!");
+        }
+    }
+  public void refuel(double amount){
+        if(fuelLevel > 0) {
+            double addedfuel = 100.0;
+            this.fuelLevel += addedfuel;
+        }
+    }
+
+    public void display_information(){
+        System.out.println("Car Information:");
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Color: " + color);
+        System.out.println("Fuel Level: " + fuelLevel);
+        System.out.println("Is the Engine Running: "+engineRunning+" " );
+    }
 }
 
-public void setModel(int model){
- this.model = model;
+public class Client extends Person {
+    protected Car car;
+
+    public Client (String name, int age, String placeofbirth, Car car){
+        super(name, age, placeofbirth);
+        this.car = car;
+    }
+
+    public Car getCar(){
+        return this.car;
+    }
+    public void setCar(Car car){
+        this.car = car;
+    }
+ @Override
+public void introduce_yourself(){
+        super.introduce_yourself();
+        System.out.println("The model of my car is "+ car.getModel() +"with the color "+car.getColor()+" " );
 }
-public void setColor(String color){
- this.color = color;
+public void client_type(){
+        if (car.getYear() > 2010){
+            System.out.println("I am the new client!");
+        } else {
+            System.out.println("I am the old client!");
+        }
 }
-public void setFuelLevel(int fuelLevel){
- this.fuelLevel = fuelLevel;
+public void test_car(){
+    car.display_information();
+    car.startEngine();
+    car.refuel(20.0);
+    car.startEngine();
+    car.display_information();
+    car.stopEngine();
 }
-public void setEngineRunning(boolean engineRunning){
- this.engineRunning = enginerunning;
+
 }
-//methods 
-public void startEngine(){
- if(this.engineRunning = false && fuelLevel > 0){
-  this.engineRunning = true;
- } else if (this.engineRunning = true) {
-  System.out.println("Engine is running!")
- } else   
- 
+public class Main {
+    public static void main(String[] args) {
+     Car newcar = new Car("Mercedes", 2022 , "black", 0,false);
+        Car oldcar = new Car ("Toyota", 2000, "red", 0, false);
+
+        Client newClient = new Client("Franz", 27, "Frankfurt", newcar);
+        Client oldClient = new Client("Felix", 35, "Darmstadt", oldcar );
+
+        newClient.introduce_yourself();
+        newClient.client_type();
+        newClient.test_car();
+
+        System.out.println();
+
+        oldClient.introduce_yourself();
+        oldClient.client_type();
+        oldClient.test_car();
+
+        System.out.println();
+
+    }
+}
  
  
